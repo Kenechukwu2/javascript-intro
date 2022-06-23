f733a3178e8c982e341443ed23c5d6fef76d315e
Kenechukwu Kingsley Nwobu <nwobukenechukwu@gmail.com>
2022-06-23 21:40:36 +0100

    Add files via upload

---
+++ Javascript intro/scripts.js
@@ -0,0 +1,7 @@
+var Fullname = " Name: Kenechukwu Kingsley Nwobu";
+var myHeight = " Height: 6ft 4inch";
+var myCountry = " Country: Nigeria";
+
+document.getElementById("name").innerHTML = Fullname;
+document.getElementById("height").innerHTML = myHeight;
+document.getElementById("country").innerHTML = myCountry;
